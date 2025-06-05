4f65f66f6fc208d07a0958f3e9ab6070e65500c7
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-06-05 14:04:34 +0300

    0. Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,23 @@
+# Another Way
+# def perform_operation(num1, num2, operation):
+#     num1 = float(num1)
+#     num2 = float(num2)
+#     operation_dictionary = {
+#         "add": num1 + num2,
+#         "subtract": num1 - num2,
+#         "multiply": num1 * num2,
+#         "divide": num1 / num2 if num2 != 0 else None
+#     }
+#     return operation_dictionary.get(operation)
+
 def perform_operation(num1, num2, operation):
-    num1 = float(num1)
-    num2 = float(num2)
-    operation_dictionary = {
-        "add": num1 + num2,
-        "subtract": num1 - num2,
-        "multiply": num1 * num2,
-        "divide": num1 / num2 if num2 != 0 else None
-    }
-    return operation_dictionary.get(operation)
-
-
+        num1 = float(num1)
+        num2 = float(num2)
+        if operation == "add":
+            return num1 + num2
+        if operation == "subtract":
+            return num1 - num2
+        if operation == "multiply":
+            return num1 * num2
+        if operation == "divide":
+            return num1 / num2 if num2 != 0 else None
