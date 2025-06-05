7c184df49866ba604c2905696e7d630ea4ef562f
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-06-05 14:07:03 +0300

    0. Arithmetic Operations Function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -20,4 +20,8 @@ def perform_operation(num1, num2, operation):
         elif operation == "multiply":
             return num1 * num2
         elif operation == "divide":
-            return num1 / num2 if num2 != 0 else None
+            if num2 != 0: 
+                return num1 / num2
+            else:
+                 return None
+
