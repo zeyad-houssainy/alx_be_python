a95dd2aac7508643418f1bc7852c7cc49bc0ccb8
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-06-05 15:01:40 +0300

    1. Shopping List Manager

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -14,12 +14,12 @@ def main():
 
         if choice == '1':
             # Prompt for and add an item
-            item = input("Enter the item to add:")
+            item = input("Enter the item to add: ")
             shopping_list.append(item)
             pass
         elif choice == '2':
             # Prompt for and remove an item
-            item = input("Enter the item to remove:")
+            item = input("Enter the item to remove: ")
             shopping_list.remove(item)
             pass
         elif choice == '3':
