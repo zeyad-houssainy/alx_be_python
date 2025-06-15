7ac076376c68023e1cda0a19d8f3195468447889
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-06-15 21:44:42 +0300

    fast commit

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,11 +1,13 @@
 class BankAccount:
     def __init__(self, account_balance=0):
+        if not(account_balance)
         self.account_balance = account_balance
 
 # e33ml ininstance w eno yb2a positive a kbr mn zero, b3den shof el withdraw wel deposit 
     def deposit(self, amount):
         self.account_balance = self.account_balance + amount
         print(f"Deposited: ${amount}")
+
 
     def withdraw(self, amount):
         if amount >= self.account_balance:
@@ -16,4 +18,4 @@ class BankAccount:
             return False
         
     def display_balance(self):
-        print(self.account_balance)
+        print(f"Current Balance:: ${self.account_balance}")
