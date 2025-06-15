671e30a7d66e7f23e4edc0ff2685def7e41613c9
zeyad-houssainy <zeyad-houssainy@hotmail.com>
2025-06-15 21:54:17 +0300

    fast commit

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,7 @@
 class BankAccount:
     def __init__(self, account_balance=0):
-        if not(account_balance)
+        if not(account_balance, (int, float)) or account_balance < 0:
+            raise ValueError("Initial balance should be a positive number")
         self.account_balance = account_balance
 
 # e33ml ininstance w eno yb2a positive a kbr mn zero, b3den shof el withdraw wel deposit 
@@ -8,14 +9,14 @@ class BankAccount:
         self.account_balance = self.account_balance + amount
         print(f"Deposited: ${amount}")
 
-
     def withdraw(self, amount):
         if amount >= self.account_balance:
             self.account_balance = self.account_balance - amount
             print(f"Withdrew: ${amount}")
             return True
         else:
+            print("Insufficient funds.")
             return False
-        
+
     def display_balance(self):
         print(f"Current Balance:: ${self.account_balance}")
